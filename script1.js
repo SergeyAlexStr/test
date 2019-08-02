55b7c0dfa6e006edf0301e38e81538f6feb2beb1
Sergey <green1309@mail.ru>
2019-08-02 11:09:52 +0300

    Это первый коммит

---
+++ script1.js
@@ -0,0 +1,11 @@
+console.log("string"*6);
+let m = 5;
+console.log(m);
+
+let arr = ['apple.bmp', 'orange.jpg', 'pinapple.jpeg'];
+console.log(arr[1]);
+
+//alert('Hello World');
+
+let answer = prompt('Есть ли вам 18?', 'Да');
+console.log(typeof(arr));
